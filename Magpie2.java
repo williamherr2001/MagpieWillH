4357d1c1a707fd606864dd49c25e0471b0ded4fb
williamherr2001 <williamherr2001@gmail.com>
2017-11-15 13:10:50 -0500

    Add files via upload

---
+++ Magpie2.java
@@ -0,0 +1,85 @@
+
+public class Magpie2 {
+
+	
+		
+			/**
+			 * Get a default greeting 	
+			 * @return a greeting
+			 */
+			public String getGreeting()
+			{
+				return "Hello, let's talk.";
+			}
+			
+			/**
+			 * Gives a response to a user statement
+			 * 
+			 * @param statement
+			 *            the user statement
+			 * @return a response based on the rules given
+			 */
+			public String getResponse(String statement)
+			{
+				String response = "";
+				if (statement.trim().length()==0) response = "Say something, please.";
+				else if (statement.indexOf("no") >= 0)
+				{
+					response = "Why so negative?";
+				}
+				else if (statement.indexOf("mother") >= 0
+						|| statement.indexOf("father") >= 0
+						|| statement.indexOf("sister") >= 0
+						|| statement.indexOf("brother") >= 0)
+				{
+					response = "Tell me more about your family.";
+				}else if (statement.indexOf("Did you know") >=0)response ="Interesting, tell me more.";
+				else if(statement.indexOf(" dog")>=0 || statement.indexOf(" cat")>=0)response = "Tell me more about your pets.";
+				else if(statement.indexOf("Dencker")>=0) response = "He sounds like a good teacher.";
+				else if(statement.indexOf("My name is ")>=0 || statement.indexOf("They call me")>=0)response = "That is a great name.";
+				else
+				{
+					response = getRandomResponse();
+				}
+				return response;
+			}
+
+			/**
+			 * Pick a default response to use if nothing else fits.
+			 * @return a non-committal string
+			 */
+			private String getRandomResponse()
+			{
+				final int NUMBER_OF_RESPONSES = 6;
+				double r = Math.random();
+				int whichResponse = (int)(r * NUMBER_OF_RESPONSES);
+				String response = "";
+				
+				if (whichResponse == 0)
+				{
+					response = "Interesting, tell me more.";
+				}
+				else if (whichResponse == 1)
+				{
+					response = "Hmmm.";
+				}
+				else if (whichResponse == 2)
+				{
+					response = "Do you really think so?";
+				}
+				else if (whichResponse == 3)
+				{
+					response = "You don't say.";
+				} else if (whichResponse == 4){
+					response = "I sure didn't know that.";
+				}else if (whichResponse == 5){
+					response = "Please explain.";
+				}
+
+				return response;
+			}
+	
+
+	}
+
+
